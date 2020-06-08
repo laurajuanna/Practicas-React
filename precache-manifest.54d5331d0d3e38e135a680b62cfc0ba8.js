abb26f747067da93eb172d5a4b1b97994f7471de
laurajuanna <juiz.laurai@gmail.com>
2020-06-08 05:15:26 -0300

    Updates

---
+++ precache-manifest.54d5331d0d3e38e135a680b62cfc0ba8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10d2630d64b7df6b8a45e20f7afabb03",
+    "url": "/practicas-react/index.html"
+  },
+  {
+    "revision": "38431643cadfc1480343",
+    "url": "/practicas-react/static/css/main.fe6cbd5f.chunk.css"
+  },
+  {
+    "revision": "df556c4a0d280a019a35",
+    "url": "/practicas-react/static/js/2.0dba6f64.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/practicas-react/static/js/2.0dba6f64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38431643cadfc1480343",
+    "url": "/practicas-react/static/js/main.11bd4cc6.chunk.js"
+  },
+  {
+    "revision": "d9df3520150e26d894ef",
+    "url": "/practicas-react/static/js/runtime-main.47161dd6.js"
+  }
+]);
